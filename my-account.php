488d2c0e1a1b80e9b65939b113f269f49d0e1bbb
Takis Pan <takispan@live.com>
2020-04-06 20:07:54 +0200

    Enqueue assets in nakamas-functions - Removed extra css files - Added tabs functionality

--- my-account.php
+++ my-account.php
@@ -16,9 +16,9 @@ get_header(); // Loads the header.php template. ?>
         <?php else : ?>
             <!-- Tab links -->
             <div class="tab">
-                <button class="tablinks" onclick="openCity(event, 'dashboard')">Dashboard</button>
-                <button class="tablinks" onclick="openCity(event, 'dance-school')">Dance School</button>
-                <button class="tablinks" onclick="openCity(event, 'profile')">Profile</button>
+                <button class="tablinks" onclick="openTab(event, 'dashboard')">Dashboard</button>
+                <button class="tablinks" onclick="openTab(event, 'dance-school')">Dance School</button>
+                <button class="tablinks" onclick="openTab(event, 'profile')">Profile</button>
             </div>
 
             <div id="dashboard" class="tabcontent">
@@ -41,32 +41,4 @@ get_header(); // Loads the header.php template. ?>
 
 </section><!-- #content -->
 
-<?php
-/* add_action to... well add our action to wp
- * Parameters
- * Required: tag (wp specific, don't change), function_to_add (name of our function)
- * Optional: priority (defaults to 10. Lower = more priority), accepted_args (accepted arguments, defaults to 1)
- */
-add_action('wp_enqueue_scripts', 'nkms_assets');
-function nkms_assets() {
-    /* Register the script like this for a plugin
-     * Parameters
-     * Required: handle (name), src (source)
-     * Optional: deps (dependencies), ver (version, used date for this), in_footer (load on footer or not. By default off an loads on header)
-     */
-    wp_register_script( 'nkms-js', plugins_url( '/assets/js/nakamas-members.js', __FILE__ ), array( 'jquery' ), '20200406', true );
-
-    /* Register the style like this for a plugin
-     * Parameters
-     * Required: handle (name), src (source)
-     * Optional: deps (dependencies), ver (version, used date for this), media (The media for which this stylesheet has been defined. Accepts media types like 'all', 'print' and 'screen', or media queries like '(orientation: portrait)' and '(max-width: 640px)'.)
-     */
-    wp_register_style( 'nkms-css', plugins_url( '/assets/css/nakamas-members.css', __FILE__ ), array(), '20200404', 'all' );
-
-    // For either a plugin or a theme, you can then enqueue the script/style
-    wp_enqueue_script( 'nkms-js' );
-    wp_enqueue_style( 'nkms-css' );
-    //wp_enqueue_script( 'nakamas-members-scripts', plugin_dir_path( __FILE__ ) . 'nakamas-members-script.js', array( 'jquery' ) );
-}
-
-get_footer(); // Loads the footer.php template. ?>
+<?php get_footer(); // Loads the footer.php template. ?>
