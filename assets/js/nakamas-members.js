f814daafb09df5ca5ada734075c28b005f464537
Takis Pan <takispan@live.com>
2020-04-10 21:31:17 +0200

    dashboard - recent orders added - reverted default tab (js) - removed unecessary code from tab-user-profile

--- assets/js/nakamas-members.js
+++ assets/js/nakamas-members.js
@@ -17,4 +17,7 @@ function openTab(evt, tabName) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
+
+  // Default tab - doesn't work for some reason, check later
+  //document.getElementById("defaultOpen").click();
 }
--- my-account.php
+++ my-account.php
@@ -5,25 +5,28 @@
  * Tabs with information & actions regarding user's account.
  */
 
+//Get user info.
+global $current_user, $wp_roles;
+wp_get_current_user();
+
 get_header(); // Loads the header.php template. ?>
 
 <section id="content">
     <div id="nkms-account">
         <?php if ( !is_user_logged_in() ) : ?>
             <p class="warning">
-                <?php _e('You must be logged to access your account', 'profile'); ?>
+                <?php _e('You must be logged in to access your account', 'profile'); ?>
             </p><!-- .warning -->
         <?php else : ?>
             <!-- Tab links -->
             <div class="tab">
-                <button class="tablinks" onclick="openTab(event, 'dashboard')">Dashboard</button>
+                <button class="tablinks" onclick="openTab(event, 'dashboard')" id="defaultOpen">Dashboard</button>
                 <button class="tablinks" onclick="openTab(event, 'dance-school')">Dance School</button>
                 <button class="tablinks" onclick="openTab(event, 'profile')">Profile</button>
             </div>
 
             <div id="dashboard" class="tabcontent">
-             <h3>Dashboard</h3>
-             <p>Dashboard stuff.</p>
+             <?php include( plugin_dir_path( __FILE__ ) . 'tabs/tab-dashboard.php'); ?>
             </div>
 
             <div id="dance-school" class="tabcontent">
@@ -33,7 +36,6 @@ get_header(); // Loads the header.php template. ?>
 
             <!-- Tab content -->
             <div id="profile" class="tabcontent">
-             <h3>Profile</h3>
              <?php include( plugin_dir_path( __FILE__ ) . 'tabs/tab-user-profile.php'); ?>
             </div>
         <?php endif; ?>
